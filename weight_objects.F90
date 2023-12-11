9ba8dffd12f04114b623d2050cb47334d25d0778
Shikhar Rai <ckhr.rai@gmail.com>
2023-12-10 21:43:35 -0500

    test for f2py

--- weight_objects.F90
+++ weight_objects.F90
@@ -15,26 +15,21 @@ module weight_objects
 
     end type pointWeight
 
-    interface pointWeight
-        MODULE PROCEDURE :: constructor
-    end interface pointWeight
-
     contains
 
-        function constructor(i, j) result(this)
-            integer, intent(in) :: i,j
-            type(pointWeight) :: this
-            this%point_i_index = i
-            this%point_j_index = j
-        end function
-
-        subroutine fillIn(this, n , i_indices, j_indices, weights )
+        subroutine fillIn(this, i, j,  n , i_indices, j_indices, weights )
             class(pointWeight), intent(inout) :: this
+            integer, intent(in) :: i,j
+            
             integer, intent(in):: n
             integer, intent(in) :: i_indices(n), j_indices(n)
             real(kind = real_kind), intent(in) :: weights(n)
 
             integer :: i_err
+
+            this%point_i_index = i
+            this%point_j_index = j
+
             allocate(this%out1d_i_index(n), &
                      this%out1d_j_index(n), &
                      this%out1d_weight(n), stat =i_err)
